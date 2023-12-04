56366be7baafb488b3ee439631a0abb9b44832ca
LAKSHMI A <108331858+ALPV488@users.noreply.github.com>
2023-12-04 19:27:53 +0530

    Add files via upload

---
+++ classexamples/collections/ArrayEx.java
@@ -0,0 +1,18 @@
+package com.collections;
+
+public class ArrayEx {
+
+	public static void main(String args[]) {
+		
+//		int arrname[] = {10,2,3,4,5,6,7};
+	
+		int arrname[] = new int[3];	
+		
+		for(int i=0;i<arrname.length;i++) {
+			
+			System.out.println(arrname[i]);
+		}
+		
+	}
+
+}
